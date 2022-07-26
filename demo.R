731f74e6105365195aee248a44be130afb6c13bd
Matthew Miesle <matthew.miesle@mssm.edu>
2022-07-26 10:07:34 -0400

    added more notes

--- demo.R
+++ demo.R
@@ -1,5 +1,21 @@
 
-# make use of some pacakges
+# project was started on GitHub
+
+# make use of some packages
 
 # library(zoo)
 # lubridate::mdy("07/26/2022")
+
+# use of Update in the Packages pane
+# use of the x button next to packages in the Packages pane
+
+# renv::status() reports differences between the project and the lockfile
+# renv::dependencies() look at the dependencies that renv is recognizing
+# renv::history() uses git to look at previous versions of lockfile
+# renv::revert() go back to previous git commit
+
+# renv::install() install particular a package for the project
+# renv::update() update packages in the project
+# renv::deactivate() removed infrastructure that automatically starts renv
+#                    when opening a project
+# renv::remove() remove package
